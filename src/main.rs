237bd33de12cf206d5b01a88e438f1060c81dbff
Cypress Frankenfeld <iamcypress@gmail.com>
2023-04-22 10:46:09 -0400

    Add skeleton rust app

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::env::var;
+
+use actix_web::{
+    get, middleware, post,
+    web::{self, Data},
+    App, HttpServer, Responder,
+};
+use env_logger::Env;
+use serde::Deserialize;
+use sqlx::PgPool;
+
+#[derive(sqlx::FromRow, Debug, Deserialize)]
+struct DataRow {
+    pub id: i32,
+    pub value: f32,
+}
+
+#[get("/data")]
+async fn get_all_data(data: Data<PgPool>) -> impl Responder {
+    let result = sqlx::query_as!(DataRow, "SELECT * FROM data")
+        .fetch_all(&**data)
+        .await
+        .unwrap();
+
+    format!("{:?}", result)
+}
+
+#[post("/data/{id}/{value}")]
+async fn post_data(data: Data<PgPool>, info: web::Path<DataRow>) -> impl Responder {
+    sqlx::query!(
+        "INSERT INTO data (id, value) VALUES ($1, $2)",
+        info.id,
+        info.value
+    )
+    .execute(&**data)
+    .await
+    .unwrap();
+
+    format!("Inserted data with id: {}, value: {}", info.id, info.value)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let database_url = var("DATABASE_URL").unwrap();
+    let port = var("PORT").unwrap();
+    let pool = PgPool::connect(&database_url).await.unwrap();
+    let data = Data::new(pool);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(data.clone())
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").to(|| async { "Hello world!" }))
+    })
+    .bind(format!("0.0.0.0:{port}"))?
+    .run()
+    .await
+}
